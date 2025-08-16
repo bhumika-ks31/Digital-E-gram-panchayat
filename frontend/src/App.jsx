6b4cc937bedf1f8e0720c4a059a0680e6a9f8a87
Bhumika kashyap <tilantajagra@gmail.com>
2025-08-16 19:16:26 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Notices from "./pages/Notices";
-import Login from "./Pages/Dashboard";
+import Dashboard from "./Pages/Dashboard";
 import Register from "./pages/Register";
 import ServiceDetail from "./pages/ServiceDetail";
 
